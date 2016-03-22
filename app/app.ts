72649634aa860518f95c95ff2595cc99c2627ca7
Michael Chan <waifung0207@gmail.com>
2016-03-22 17:52:49 +0800

    Included essential plugins and Crosswalk

--- app/app.ts
+++ app/app.ts
@@ -4,17 +4,17 @@ import {StatusBar} from 'ionic-native';
 import {HomePage} from './pages/home/home';
 
 @App({
-  template: '<ion-nav [root]="rootPage"></ion-nav>',
-  config: {} // http://ionicframework.com/docs/v2/api/config/Config/
+	template: '<ion-nav [root]="rootPage"></ion-nav>',
+	config: {} // http://ionicframework.com/docs/v2/api/config/Config/
 })
 export class MyApp {
-  rootPage: any = HomePage;
+	rootPage: any = HomePage;
 
-  constructor(platform: Platform) {
-    platform.ready().then(() => {
-        // Okay, so the platform is ready and our plugins are available.
-        // Here you can do any higher level native things you might need.
-        StatusBar.styleDefault();
-    });
-  }
+	constructor(platform: Platform) {
+		platform.ready().then(() => {
+			// Okay, so the platform is ready and our plugins are available.
+			// Here you can do any higher level native things you might need.
+			StatusBar.styleDefault();
+		});
+	}
 }
