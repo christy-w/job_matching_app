27a595ad812662b0b6067b20891f7cde4d554cb2
Michael Chan <waifung0207@gmail.com>
2016-04-25 19:33:23 +0800

    Added example of Virtual Scroll to Demo List page

--- app/pages/demo-list/demo-list.ts
+++ app/pages/demo-list/demo-list.ts
@@ -1,9 +1,29 @@
 import {Page, NavController} from 'ionic-angular';
 
+/**
+ * Demonstrating Virtual Scroll for list items
+ * Reference: http://www.joshmorony.com/boosting-scroll-performance-in-ionic-2/
+ */
+
 @Page({
   templateUrl: 'build/pages/demo-list/demo-list.html'
 })
 export class DemoListPage {
-    constructor(private nav: NavController) {
+
+	private items: Object[];
+	
+	constructor(private nav: NavController) {
+
+		this.items = [];
+
+		for (let i = 0; i < 2000; i++) {
+			let item = {
+				title: 'Title',
+				body: 'body',
+				avatarUrl: 'https://avatars.io/facebook/random'+i
+			};
+
+			this.items.push(item);
+		}
     }
 }
