481175e8e920b597bfcef39b225d3605bc7a8e61
Michael Chan <waifung0207@gmail.com>
2016-10-12 15:35:16 +0800

    Update my-navbar component

--- src/components/my-navbar/my-navbar.ts
+++ src/components/my-navbar/my-navbar.ts
@@ -1,5 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef } from '@angular/core';
 
+/**
+ * Common navbar across pages 
+ *
+ * Usage:
+ *	<my-navbar [title]="'PAGE.HOME' | translate" [enable_music]="true" [enable_back]="true"></my-navbar>
+ */
 @Component({
 	selector: 'my-navbar',
 	templateUrl: 'my-navbar.html'
@@ -8,7 +14,22 @@ export class MyNavbar {
 
 	@Input() title: string;
 	@Input() logo_url: string;
+	@Input() enable_menu: boolean = false;
+	@Input() enable_back: boolean = false;
 
-	constructor() {
+	constructor(private el: ElementRef) {
+	}
+
+	// To remove unnecessary <my-navbar></my-navbar> tag
+	// Reference: http://stackoverflow.com/questions/34280475/remove-the-host-html-element-selectors-created-by-angular-component
+	ngOnInit() {
+		var nativeElement: HTMLElement = this.el.nativeElement,
+			parentElement: HTMLElement = nativeElement.parentElement;
+		// move all children out of the element
+		while (nativeElement.firstChild) {
+			parentElement.insertBefore(nativeElement.firstChild, nativeElement);
+		}
+		// remove the empty element(the host)
+		parentElement.removeChild(nativeElement);
 	}
 }
