b6087c7bd247b4cd23658a7f9533a47d57cafcbc
Michael Chan <waifung0207@gmail.com>
2017-11-18 01:51:09 +0800

    Revamp BaseService to use Fetch API instead of Angular HttpClient

--- src/providers/api.ts
+++ src/providers/api.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Platform } from 'ionic-angular';
 import { Utils } from '../core/providers/utils';
 import { BaseService } from '../core/base-service';
@@ -11,7 +10,7 @@ export class Api extends BaseService {
 	//protected api_prefix: string = 'http://localhost/juicylauncher2_web/api';
 	protected api_prefix: string = 'https://dev.juicyapphk.com/juicylauncher2_web/api';
 	
-	constructor(http: HttpClient, platform: Platform, utils: Utils) {
-		super(http, platform, utils);
+	constructor(platform: Platform, utils: Utils) {
+		super(platform, utils);
 	}
 }
