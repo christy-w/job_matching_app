4c8b78e46bccb58624a778a4a89fd406299447c6
Wong Oi In <christywong0218@gmail.com>
2018-11-01 16:45:20 +0800

    Minor modification on element style

--- src/pages/applicant-profile/applicant-profile.module.ts
+++ src/pages/applicant-profile/applicant-profile.module.ts
@@ -4,12 +4,14 @@ import { TranslateModule } from '@ngx-translate/core';
 import { ComponentsModule } from '../../components/components.module';
 import { ApplicantProfilePage } from './applicant-profile';
 
+import { StarRatingModule } from 'ionic3-star-rating';
 @NgModule({
   declarations: [
     ApplicantProfilePage
   ],
   imports: [
     ComponentsModule,
+    StarRatingModule,
     IonicPageModule.forChild(ApplicantProfilePage),
     TranslateModule.forChild()
   ]
