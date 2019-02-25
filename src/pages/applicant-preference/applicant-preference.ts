81a37df38eb776e9fcc5bcef097791cf92d92b50
Wong Oi In <christywong0218@gmail.com>
2019-02-25 17:01:49 +0800

    (pending) Modify preference page

--- src/pages/applicant-preference/applicant-preference.ts
+++ src/pages/applicant-preference/applicant-preference.ts
@@ -15,6 +15,7 @@ export class ApplicantPreferencePage extends BasePage {
 	name: string = 'ApplicantPreferencePage';
 	setting_fields: any = [];
 	preference_fields: any = [];
+	preference_options: any = {};
 
 	constructor(
 		protected platform: Platform,
@@ -25,72 +26,82 @@ export class ApplicantPreferencePage extends BasePage {
 	) {
 		super(platform, view, nav, utils);
 		Config.DEBUG_VERBOSE && console.log('ApplicantPreferencePage constructor');
-		
-		this.setting_fields = [
+
+		this.preference_options = [
 			{
-				name: '可工作類別',
-				field_type: 'multi_select', // input/select/multi_select/date/
+				name_zh: '必要',
+				name_en: 'Must'
+			},
+			{
+				name_zh: '優先',
+				name_en: 'Preferred'
+			},
+			{
+				name_zh: '毋須',
+				name_en: 'None'
+			},
+		];
+		
+		this.preference_fields = [
+			{
+				name: '工作類別',
 				selection: '',
 				options: ['全職', '兼職', '臨時工作']
 			},
 			{
-				name: '可工作地區',
-				field_type: 'multi_select', // input/select/multi_select/date/
+				name: '工作地區',
 				selection: '',
 				options: ['油尖旺區', '深水埗區', '九龍城區']
 			},
 			{
-				name: '可工作時段（適用於兼職及臨時工作）',
-				field_type: 'multi_select', // input/select/multi_select/date/
+				name: '工作時段（適用於兼職及臨時工作）',
 				selection: '',
 				options: ['早上', '下午', '晚間' ,'凌晨']
 			},
 			{
-				name: '可工作日子（適用於兼職及臨時工作）',
-				field_type: 'multi_select', // input/select/multi_select/date/
+				name: '工作日子（適用於兼職及臨時工作）',
 				selection: '',
 				options: ['星期一', '星期二', '星期三' ,'星期四', '星期五', '星期六' ,'星期日']
 			},
 			{
-				name: '可上班日期',
-				field_type: 'date', // input/select/multi_select/date/
+				name: '最低月薪薪酬（適用於全職工作）',
 				selection: '',
-				options: []
+				options: ['HKD 10000', 'HKD 15000', 'HKD 20000' ,'HKD 25000', 'HKD 30000']
 			},
 		];
 
-		this.preference_fields = [
-			{
-				name: '理想時酬（適用於兼職及臨時工作）',
-				field_type: 'input', // input/select/multi_select/date/range
-				selection: '',
-				options: []
-			},
-			{
-				name: '理想月薪（適用於全職工作）',
-				field_type: 'input', // input/select/multi_select/date/range
-				selection: '',
-				options: []
-			},
-			{
-				name: '公司產業',
-				field_type: 'multi_select', // input/select/multi_select/date/range
-				selection: '',
-				options: ['餐飲業', '酒店業', '展覽業', '運輸業', '零售業']
-			},
-			{
-				name: '公司規模',
-				field_type: 'multi_select', // input/select/multi_select/date/range
-				selection: '',
-				options: ['1-25人', '25-100人', '100-1000人', '1000人以上']
-			},
-			{
-				name: '出糧方式',
-				field_type: 'multi_select', // input/select/multi_select/date/range
-				selection: '',
-				options: ['過數出糧', '現金出糧', '支票出糧']
-			},
-		];
+		// this.preference_fields = [
+		// 	{
+		// 		name: '理想時酬（適用於兼職及臨時工作）',
+		// 		field_type: 'input', // input/select/multi_select/date/range
+		// 		selection: '',
+		// 		options: []
+		// 	},
+		// 	{
+		// 		name: '理想月薪（適用於全職工作）',
+		// 		field_type: 'input', // input/select/multi_select/date/range
+		// 		selection: '',
+		// 		options: []
+		// 	},
+		// 	{
+		// 		name: '公司產業',
+		// 		field_type: 'multi_select', // input/select/multi_select/date/range
+		// 		selection: '',
+		// 		options: ['餐飲業', '酒店業', '展覽業', '運輸業', '零售業']
+		// 	},
+		// 	{
+		// 		name: '公司規模',
+		// 		field_type: 'multi_select', // input/select/multi_select/date/range
+		// 		selection: '',
+		// 		options: ['1-25人', '25-100人', '100-1000人', '1000人以上']
+		// 	},
+		// 	{
+		// 		name: '出糧方式',
+		// 		field_type: 'multi_select', // input/select/multi_select/date/range
+		// 		selection: '',
+		// 		options: ['過數出糧', '現金出糧', '支票出糧']
+		// 	},
+		// ];
 	}
 
 	ionViewWillEnter() {
