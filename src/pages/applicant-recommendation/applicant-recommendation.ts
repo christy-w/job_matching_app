1ab121a02b743db5c1643423e59bd0b9a4abda39
Wong Oi In <christywong0218@gmail.com>
2019-03-10 17:24:51 +0800

    Allow filter MUST with both input and multi-select fields

--- src/pages/applicant-recommendation/applicant-recommendation.ts
+++ src/pages/applicant-recommendation/applicant-recommendation.ts
@@ -20,9 +20,11 @@ export class ApplicantRecommendationPage extends BasePage {
 	all_jobs: any;
 	recommendations: any = [];
 
-	// must_pref: any = [];
-	// preferred_pref: any = [];
-	// other_pref: any = [];
+	must_pref: any = [];
+	preferred_pref: any = [];
+	other_pref: any = [];
+
+
 
 	must_jobs: any = [];
 	preferred_jobs: any = [];
@@ -168,83 +170,40 @@ export class ApplicantRecommendationPage extends BasePage {
 		let must_pref = _.filter(this.user_preference, {'importance':2})
 		console.log('must_pref', must_pref);
 
+		let preferred_pref = _.filter(this.user_preference, {'importance':1});
+		console.log('preferred_pref', preferred_pref);
+
 		// Get jobs fulfilled MUST condition
-		this.getMustJobs(must_pref);
-		// this.getPreferredJobs(preferred_pref);
-		// this.getOtherJobs(none_pref);
-		// _.each(this.user_preference, (pref) => {
-		// 	// Key to match with job key
-		// 	let match_key = this.match.translatePrefKey(pref.value);
-		// 	// All preferences
-		// 	let match_all_value = this.match.translatePrefAllValue(pref.value);
-		// 	// User preferences that are selected
-		// 	let match_value = this.match.translatePrefValue(pref.value, pref.selection);
-		// 	// User preferences that are not selected
-		// 	let other_value = _.difference(match_all_value, match_value);
-
-		// 	if (pref.importance == 2) {
-		// 		// For fields considered as MUST
-		// 		let must_jobs = [];
-		// 		// Check if preference match with job, if not, filter from list
-		// 		_.each(match_value, (value) => {
-		// 			let matched_jobs = _.filter(this.all_jobs, function(job) {
-		// 				return job[match_key] == value;
-		// 			});
-		// 			must_jobs = _.concat(must_jobs, matched_jobs)
-		// 		})
-		// 		console.log('must_jobs', must_jobs);
-		// 	} else if (pref.importance == 1) {
-		// 		// For fields considered as PREFERRED
-		// 		let preferred_jobs = [];
-		// 		_.each(match_value, (value) => {
-		// 			// Place preferref jobs first, then the others
-		// 			let matched_jobs = _.partition(this.all_jobs, function(job) {
-		// 				return job[match_key] == value;
-		// 			});
-		// 			preferred_jobs = _.flatten(matched_jobs);
-		// 			console.log('preferred_jobs', preferred_jobs);
-		// 		})
-		// 	} else {
-
-		// 	}
-		// })
+		if (must_pref && must_pref.length > 0) {
+			this.getMustJobs(must_pref);
+		} else if (preferred_pref && preferred_pref.length > 0){
+			this.getPreferredJobs(preferred_pref)
+		} else {
+			console.log('initRecommendations > no pref');
+			this.user_preference = null;
+		}
 	}
 
 	getMustJobs(must_pref) {
+		// For multi-select typed pref
+		let must_selects_pref = _.filter(must_pref, {'field_type': 'multi_select'});
+		let must_input_pref = _.filter(must_pref, {'field_type': 'input'});
 
-		_.each(must_pref, (pref) => {
-			// console.log('pref', pref.value);
-			// Key to match with job key
-			let match_key = this.match.translatePrefKey(pref.value);
-			// User preferences that are selected
-			let match_value = this.match.translatePrefValue(pref.value, pref.selection);
-			
-			// Check if preference match with job, if not, filter from list
-			let must_jobs_per_pref = [];
-			_.each(match_value, (value) => {
-				// console.log('value', value);
-				let matched_jobs_per_pref = _.filter(this.all_jobs, function(job) {
-					if (!job.match_item) {
-						job.match_item = [];
-					}
-					if (job[match_key] == value) {
-						job.match_item.push({ 'name': match_key, 'content': value });
-					}
-					return job[match_key] == value;
-				});
-				// console.log('matched', matched_jobs_per_pref);
-				must_jobs_per_pref = _.concat(must_jobs_per_pref, matched_jobs_per_pref)
-			})
-			this.must_jobs = must_jobs_per_pref;
-		});
-		console.log('must_jobs', this.must_jobs);
+		if (must_selects_pref && must_selects_pref.length > 0) {
+			this.getMustSelects(must_selects_pref);
+			if (must_input_pref && must_input_pref.length > 0) {
+				this.getMustInput(must_input_pref);
+			}
+		} else if (must_input_pref && must_input_pref.length > 0){
+			this.getMustInput(must_input_pref);
+		}
 
 		if (this.checkLength(this.must_jobs)) {
 			// Pick PREFFERED to fulfilled preferences
 			let preferred_pref = _.filter(this.user_preference, {'importance':1});
 			console.log('preferred_pref', preferred_pref);
 			
-			if (preferred_pref.length > 0) {
+			if (preferred_pref && preferred_pref.length > 0) {
 				this.getPreferredJobs(preferred_pref);
 			} else {
 				this.recommendations = this.must_jobs;
@@ -256,27 +215,112 @@ export class ApplicantRecommendationPage extends BasePage {
 		}
 	}
 
-	getPreferredJobs(preferred_pref) {
-		_.each(preferred_pref, (pref) => {
+	getMustSelects(must_selects_pref) {
+		_.each(must_selects_pref, (pref) => {
 			// Key to match with job key
 			let match_key = this.match.translatePrefKey(pref.value);
-			// All preferences
-			let match_all_value = this.match.translatePrefAllValue(pref.value);
 			// User preferences that are selected
 			let match_value = this.match.translatePrefValue(pref.value, pref.selection);
-			// User preferences that are not selected
-			let other_value = _.difference(match_all_value, match_value);
 
+			// Check if preference match with job, if not, filter from list
+			let must_jobs_per_pref = [];
+			_.each(match_value, (value) => {
+				// console.log('value', value);
+				let matched_jobs_per_pref = _.filter(this.all_jobs, function(job) {
+					if (!job.match_item) {
+						job.match_item = [];
+					}
+					if (job[match_key] == value) {
+						job.match_item.push({ 'name': match_key, 'content': value, 'importance': 2 });
+					}
+					return job[match_key] == value;
+				});
+				// console.log('matched', matched_jobs_per_pref);
+				must_jobs_per_pref = _.concat(must_jobs_per_pref, matched_jobs_per_pref)
+			})
+			this.must_jobs = must_jobs_per_pref;
+		});
+		console.log('multi-select must_jobs', this.must_jobs);
+	}
+
+	getMustInput(must_input_pref) {
+		let ft_above_wage = [];
+		let non_ft_above_wage = [];
+		_.each(must_input_pref, (pref) => {
+			// Key to match with job key
+			let match_key = this.match.translatePrefKey(pref.value);
+			let match_value = pref.selection;
+			let reformatted_ft_jobs = _.partition(this.must_jobs, {'type': 'fulltime'})[0];
+			let reformatted_non_ft_jobs = _.partition(this.must_jobs, {'type': 'fulltime'})[1];
+
+			ft_above_wage =  _.filter(reformatted_ft_jobs, function(o) {
+				if (match_key == 'monthly_wage') {
+					o['match_item'].push({ 'name': 'monthly_wage', 'content': match_value, 'importance': 2 });
+				}
+				return o['monthly_wage'] >= match_value;
+			})
+
+			ft_above_wage = _.orderBy(ft_above_wage, ['monthly_wage'], ['desc']);
+			console.log('ft_above_wage', ft_above_wage);
+
+			non_ft_above_wage =  _.filter(reformatted_non_ft_jobs, function(o) {
+				if (match_key == 'hourly_wage') {
+					o['match_item'].push({ 'name': 'hourly_wage', 'content': match_value, 'importance': 2 });
+				}
+				return o['hourly_wage'] >= match_value;
+			})
+
+			non_ft_above_wage = _.orderBy(non_ft_above_wage, ['hourly_wage'], ['desc']);
+			console.log('non_ft_above_wage', non_ft_above_wage);
+		})
+		this.must_jobs = _.concat(ft_above_wage, non_ft_above_wage);
+		console.log('input must_jobs', this.must_jobs);
+	}
+
+	getPreferredJobs(preferred_pref) {
+		// For multi-select typed pref
+		let preferred_selects_pref = _.filter(preferred_pref, {'field_type': 'multi_select'});
+		let preferred_input_pref = _.filter(preferred_pref, {'field_type': 'input'});
+
+		if (preferred_selects_pref && preferred_selects_pref.length > 0) {
+			this.getPreferredSelects(preferred_selects_pref);
+			if (preferred_input_pref && preferred_input_pref.length > 0) {
+				this.getPreferredInput(preferred_input_pref);
+			}
+		} else if (preferred_input_pref && preferred_input_pref.length > 0){
+			this.getPreferredInput(preferred_input_pref);
+		}
+	}
+
+	getPreferredSelects(preferred_selects_pref) {
+		_.each(preferred_selects_pref, (pref) => {
+			// Key to match with job key
+			let match_key = this.match.translatePrefKey(pref.value);
+			// User preferences that are selected
+			let match_value = this.match.translatePrefValue(pref.value, pref.selection);
+
+			// Check if preference match with job, if not, filter from list
+			let preferred_jobs_per_pref = [];
 			// For fields considered as PREFERRED
 			_.each(match_value, (value) => {
 				// Place preferref jobs first, then the others
-				let matched_jobs = _.partition(this.must_jobs, function(job) {
+				let matched_jobs_per_pref = _.partition(this.must_jobs, function(job) {
+					if (!job.match_item) {
+						job.match_item = [];
+					}
+					if (job[match_key] == value) {
+						job.match_item.push({ 'name': match_key, 'content': value, 'importance': 1 });
+					}
 					return job[match_key] == value;
 				});
-				this.preferred_jobs = _.flatten(matched_jobs);
-				console.log('preferred_jobs', this.preferred_jobs);
+				preferred_jobs_per_pref = _.flatten(matched_jobs_per_pref);
 			})
+			this.preferred_jobs = preferred_jobs_per_pref;
+			console.log('preferred_jobs', this.preferred_jobs);
 		})
+	}
+ 
+	getPreferredInput(preferred_input_pref) {
 
 	}
 
