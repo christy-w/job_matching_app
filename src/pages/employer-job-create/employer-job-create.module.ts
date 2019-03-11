fcd217484a8dcc1647ae635f8482913942b3b7bd
Wong Oi In <christywong0218@gmail.com>
2019-03-12 02:26:27 +0800

    (Pending) Build employer job create page

--- src/pages/employer-job-create/employer-job-create.module.ts
+++ src/pages/employer-job-create/employer-job-create.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+import { TranslateModule } from '@ngx-translate/core';
+import { ComponentsModule } from '../../components/components.module';
 import { EmployerJobCreatePage } from './employer-job-create';
 
 @NgModule({
   declarations: [
-    EmployerJobCreatePage,
+    EmployerJobCreatePage
   ],
   imports: [
+    ComponentsModule,
     IonicPageModule.forChild(EmployerJobCreatePage),
-  ],
+    TranslateModule.forChild()
+  ]
 })
 export class EmployerJobCreatePageModule {}
