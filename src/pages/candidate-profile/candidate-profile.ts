ea8f5fc8a517ed6d17c3d7bc26a0784a75c09e15
Wong Oi In <christywong0218@gmail.com>
2019-03-18 12:40:17 +0800

    Add skills, lang and certs field on candidate profile

--- src/pages/candidate-profile/candidate-profile.ts
+++ src/pages/candidate-profile/candidate-profile.ts
@@ -27,101 +27,122 @@ export class CandidateProfilePage extends BasePage {
 	) {
 		super(platform, view, nav, utils);
     Config.DEBUG_VERBOSE && console.log('CandidateProfilePage constructor');
-    
-    let applicant_id = this.params.get('applicant_id');
+    let applicant_id = '3';
+    // let applicant_id = this.params.get('applicant_id');
     this.api.startQueue([
 			this.api.getApplicantProfile(applicant_id),
 		]).then(response => {
-      this.candidate = response[0];
-
+      let candidate = response[0];
       // Translate gender field
-      switch(this.candidate.gender) {
+      switch(candidate.gender) {
         case 'female':
-          this.candidate.gender_zh = '女';
-          this.candidate.gender_en = 'Female';
+          candidate.gender_zh = '女';
+          candidate.gender_en = 'Female';
           break;
         case 'male':
-          this.candidate.gender_zh = '男';
-          this.candidate.gender_en = 'Male';
+          candidate.gender_zh = '男';
+          candidate.gender_en = 'Male';
           break;
       }
 
       // Translate age field
-      this.candidate.age = moment().diff(moment(this.candidate.dob, "YYYY-MM-DD"), 'years');
+      candidate.age = moment().diff(moment(candidate.dob, "YYYY-MM-DD"), 'years');
 
-      switch(this.candidate.education_level) {
+      switch(candidate.education_level) {
         case 'never':
-          this.candidate.education_zh = '從未';
-          this.candidate.education_en = 'Never';
+          candidate.education_zh = '從未';
+          candidate.education_en = 'Never';
           break;
         case 'primary':
-          this.candidate.education_zh = '小學或以下';
-          this.candidate.education_en = 'Primary school or below';
+          candidate.education_zh = '小學或以下';
+          candidate.education_en = 'Primary school or below';
           break;
         case 'secondary':
-          this.candidate.education_zh = '中學';
-          this.candidate.education_en = 'Secondary school';
+          candidate.education_zh = '中學';
+          candidate.education_en = 'Secondary school';
           break;
         case 'post_secondary':
-          this.candidate.education_zh = '大專 / 副學士 / 文憑';
-          this.candidate.education_en = 'Post-secondary school / Associate Degree / Diploma';
+          candidate.education_zh = '大專 / 副學士 / 文憑';
+          candidate.education_en = 'Post-secondary school / Associate Degree / Diploma';
           break;
         case 'university':
-          this.candidate.education_zh = '大學或以上';
-          this.candidate.education_en = 'University or above';
+          candidate.education_zh = '大學或以上';
+          candidate.education_en = 'University or above';
           break;
       }
 
-      switch(this.candidate.employment_status) {
+      switch(candidate.employment_status) {
         case 'full_time':
-          this.candidate.employment_zh = '全職';
-          this.candidate.employment_en = 'Full Time';
+          candidate.employment_zh = '全職';
+          candidate.employment_en = 'Full Time';
           break;
         case 'part_time':
-          this.candidate.employment_zh = '兼職';
-          this.candidate.employment_en = 'Part Time';
+          candidate.employment_zh = '兼職';
+          candidate.employment_en = 'Part Time';
           break;
         case 'self_employed':
-          this.candidate.employment_zh = '自僱';
-          this.candidate.employment_en = 'Self Employed';
+          candidate.employment_zh = '自僱';
+          candidate.employment_en = 'Self Employed';
           break;
         case 'home_maker':
-          this.candidate.employment_zh = '主婦';
-          this.candidate.employment_en = 'Home maker';
+          candidate.employment_zh = '主婦';
+          candidate.employment_en = 'Home maker';
           break;
         case 'unemployed':
-          this.candidate.employment_zh = '待業';
-          this.candidate.employment_en = 'Unemployed';
+          candidate.employment_zh = '待業';
+          candidate.employment_en = 'Unemployed';
           break;
         case 'student':
-          this.candidate.employment_zh = '學生';
-          this.candidate.employment_en = 'Student';
+          candidate.employment_zh = '學生';
+          candidate.employment_en = 'Student';
           break;
       }
 
-
-      switch(this.candidate.related_experience) {
+      switch(candidate.related_experience) {
         case 'none':
-          this.candidate.experience_zh = '沒有經驗';
-          this.candidate.experience_en = 'None';
+          candidate.experience_zh = '沒有經驗';
+          candidate.experience_en = 'None';
           break;
         case 'half_year':
-          this.candidate.experience_zh = '半年或以內';
-          this.candidate.experience_en = '6 months or less';
+          candidate.experience_zh = '半年或以內';
+          candidate.experience_en = '6 months or less';
           break;
         case '1_year':
-          this.candidate.experience_zh = '半年至1年';
-          this.candidate.experience_en = '6 months to 1 year';
+          candidate.experience_zh = '半年至1年';
+          candidate.experience_en = '6 months to 1 year';
           break;
         case '3_year':
-          this.candidate.experience_zh = '1年至3年';
-          this.candidate.experience_en = '1 year to 3 years';
+          candidate.experience_zh = '1年至3年';
+          candidate.experience_en = '1 year to 3 years';
           break;
         case '3_year_above':
-          this.candidate.experience_zh = '3年以上';
-          this.candidate.experience_en = '3 years of more';
+          candidate.experience_zh = '3年以上';
+          candidate.experience_en = '3 years of more';
           break;
       }
+
+      candidate.language_texts = [];
+      candidate.language_abilities.forEach(lang_id => {
+        this.api.getSystemInfoItem('language_ability', lang_id).then(lang => {
+          candidate.language_texts.push(lang);
+        })
+      });
+
+      candidate.computer_texts = [];
+      candidate.computer_skills.forEach(skill_id => {
+        this.api.getSystemInfoItem('computer_skill', skill_id).then(skill => {
+          candidate.computer_texts.push(skill);
+        })
+      });
+
+      candidate.related_texts = [];
+      candidate.related_certs.forEach(cert_id => {
+        this.api.getSystemInfoItem('related_certificate', cert_id).then(cert => {
+          candidate.related_texts.push(cert);
+        })
+      });
+      
+      this.candidate = candidate;
       console.log(123, this.candidate);
     });
 	}
--- src/pages/employer-record/employer-record.ts
+++ src/pages/employer-record/employer-record.ts
@@ -4,6 +4,8 @@ import { IonicPage } from 'ionic-angular';
 import { BasePage } from '../../core/base-page';
 import { Config } from '../../config';
 import { Utils } from '../../core/providers/utils';
+import { Api } from '../../providers';
+import _ from 'lodash';
 
 @IonicPage()
 @Component({
@@ -13,12 +15,14 @@ import { Utils } from '../../core/providers/utils';
 export class EmployerRecordPage extends BasePage {
 
 	name: string = 'EmployerRecordPage';
-	
+	records: any;
+
 	constructor(
 		protected platform: Platform,
 		protected view: ViewController,
 		protected nav: NavController,
-		protected utils: Utils
+		protected utils: Utils,
+		private api: Api
 	) {
 		super(platform, view, nav, utils);
 		Config.DEBUG_VERBOSE && console.log('EmployerRecordPage constructor');
@@ -26,5 +30,160 @@ export class EmployerRecordPage extends BasePage {
 
 	ionViewWillEnter() {
 		Config.ACTIVE_TAB = 'record';
+
+		this.api.startQueue([
+			this.api.getApplicationsByUser()
+		]).then(response => {
+			let records = response[0];
+
+			_.each(records, (record) => {
+				switch (record.application_status) {
+					case 'submitted': 
+						record.status_zh = '已遞交申請';
+						record.status_en = 'Submitted';
+						record.status_color = '#fab712'; // yellow
+						record.allow_operation = 'cancel'; // cancel
+						break;
+					case 'in_progress':
+						record.status_zh = '申請處理中';
+						record.status_en = 'In progress';
+						record.status_color = '#fab712'; // yellow
+						record.allow_operation = 'cancel'; // cancel
+						break;
+					case 'offered': 
+						record.status_zh = '申請成功';
+						record.status_en = 'Offered';
+						record.status_color = '#4ebc4e'; // green
+						record.allow_operation = 'offer'; // cancel
+						break;
+					case 'accepted_offer': 
+						record.status_zh = '己接受工作';
+						record.status_en = 'Offer accepted';
+						record.status_color = '#4ebc4e'; // green
+						record.allow_operation = 'none'; // none
+						// none
+						break;
+					case 'withdrawn': 
+						record.status_zh = '職位已取消';
+						record.status_en = 'Position withdrawn';
+						record.status_color = '#f05050'; // red
+						record.allow_operation = 'none'; // none
+						// none
+						break;
+					case 'vacancy_filled': 
+						record.status_zh = '職位已滿';
+						record.status_en = 'Vacancy filled';
+						record.status_color = '#f05050'; // red
+						record.allow_operation = 'none'; // none
+						// none
+						break;
+					case 'rejected_offer': 
+						record.status_zh = '已拒絕工作';
+						record.status_en = 'Offer rejected';
+						record.status_color = '#f05050'; // red
+						record.allow_operation = 'none'; // none
+						// none
+						break;
+					case 'cancelled': 
+						record.status_zh = '已取消申請';
+						record.status_en = 'Cancelled';
+						record.status_color = '#f05050'; // red
+						record.allow_operation = 'none'; // none
+						// none
+						break;
+				}
+
+				// Format publish dates
+				let job = record.job;
+
+				// Format job type and wage
+				let monthly_wage = job.monthly_wage;
+				let hourly_wage = job.hourly_wage;
+				switch(job.type) {
+					case 'fulltime':
+						job.type_zh = '全職';
+						job.type_en = 'Full Time';
+						job.wage_zh = '$' + monthly_wage + '/月';
+						job.wage_en = '$' + monthly_wage + '/Month';
+						break;
+					case 'parttime':
+						job.type_zh = '兼職';
+						job.type_en = 'Part Time';
+						job.wage_zh = '$' + hourly_wage + '/小時';
+						job.wage_en = '$' + hourly_wage + '/Hour';
+						break;
+					case 'temporary':
+						job.type_zh = '臨時工作';
+						job.type_en = 'Temporary Work';
+						job.wage_zh = '$' + hourly_wage + '/小時';
+						job.wage_en = '$' + hourly_wage + '/Hour';
+						break;
+				}
+			})
+			this.records = _.orderBy(records, ['last_updated_at'], ['desc']);
+		})
+	}
+
+	cancelApplication(application_id) {
+		this.utils.showConfirm('', this.utils.instantLang('MSG.CANCEL_CONFIRM'), ()=>{
+			this.api.startQueue([
+				this.api.postApplicationCancel(application_id)
+			]).then(response => {
+				let cancel_response = response[0];
+				if (cancel_response['status']) {
+					// Applied successful
+					this.utils.showAlert('', this.utils.instantLang('MSG.CANCEL_SUCCESS'));
+				} else {
+					// Apply failed
+					this.utils.showAlert('', this.utils.instantLang('MSG.CANCEL_FAILED'));
+				}
+				this.nav.setRoot('ApplicantRecordPage');
+			})
+		});
+	}
+
+	respondOffer(application_id, response: boolean) {
+		if (response) {
+			let data = {
+				application_status: 'accepted_offer'
+			}
+			this.api.startQueue([
+				this.api.postApplicationUpdate(application_id, data)
+			]).then(response => {
+				let accept_response = response[0];
+				if (accept_response['status']) {
+					// Applied successful
+					this.utils.showAlert('', this.utils.instantLang('MSG.ACCEPT_SUCCESS'));
+				} else {
+					// Apply failed
+					this.utils.showAlert('', this.utils.instantLang('MSG.ACCEPT_FAILED'));
+				}
+				this.nav.setRoot('ApplicantRecordPage');
+			})
+		} else {
+			this.utils.showConfirm('', this.utils.instantLang('MSG.CANCEL_CONFIRM'), ()=>{
+				let data = {
+					application_status: 'rejected_offer'
+				}
+				this.api.startQueue([
+					this.api.postApplicationUpdate(application_id, data)
+				]).then(response => {
+					let reject_response = response[0];
+					if (reject_response['status']) {
+						// Applied successful
+						this.utils.showAlert('', this.utils.instantLang('MSG.REJECT_SUCCESS'));
+					} else {
+						// Apply failed
+						this.utils.showAlert('', this.utils.instantLang('MSG.REJECT_FAILED'));
+					}
+					this.nav.setRoot('ApplicantRecordPage');
+				})
+			});
+		}
+	}
+
+	seeJobDetail(job_id) {
+		let data = { 'job_id': job_id };
+		this.nav.push('ApplicantJobPage', data);
 	}
 }
