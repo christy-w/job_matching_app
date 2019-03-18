e49e48299ff4aafc7238f6bc81bda0e7aae4973a
Wong Oi In <christywong0218@gmail.com>
2019-03-18 21:41:10 +0800

    Connect applicant review to db

--- src/pages/applicant-review/applicant-review.ts
+++ src/pages/applicant-review/applicant-review.ts
@@ -4,6 +4,9 @@ import { IonicPage } from 'ionic-angular';
 import { BasePage } from '../../core/base-page';
 import { Config } from '../../config';
 import { Utils } from '../../core/providers/utils';
+import { Api } from '../../providers';
+import _ from 'lodash';
+import * as moment from 'moment';
 
 @IonicPage()
 @Component({
@@ -20,31 +23,47 @@ export class ApplicantReviewPage extends BasePage {
 		protected platform: Platform,
 		protected view: ViewController,
 		protected nav: NavController,
-		protected utils: Utils
+		protected utils: Utils,
+		private api: Api
 	) {
 		super(platform, view, nav, utils);
     	Config.DEBUG_VERBOSE && console.log('ApplicantReviewPage constructor');
-    
-		this.reviews = [
-			{
-				name: '黃小明',
-				publish_date: '2018-09-01',
-				rating: 4,
-				feedback: '員工做事盡責，態度友善有禮，會考慮再次僱用。'
-			},
-			{
-				name: '黃小明',
-				publish_date: '2018-08-23',
-				rating: 3,
-				feedback: '員工做事盡責，態度友善有禮，會考慮再次僱用。員工做事盡責，態度友善有禮，會考慮再次僱用。員工做事盡責，態度友善有禮，會考慮再次僱用。員工做事盡責，態度友善有禮，會考慮再次僱用。員工做事盡責，態度友善有禮，會考慮再次僱用。員工做事盡責，態度友善有禮，會考慮再次僱用。員工做事盡責，態度友善有禮，會考慮再次僱用。員工做事盡責，態度友善有禮，會考慮再次僱用。員工做事盡責，態度友善有禮，會考慮再次僱用。'
-			},
-			{
-				name: '黃小明',
-				publish_date: '2018-07-01',
-				rating: 4,
-				feedback: '員工做事盡責，態度友善有禮，會考慮再次僱用。'
+		
+		this.api.startQueue([
+			this.api.getApplicantProfile()
+		]).then(response => {
+			let applicant = response[0];
+			if (applicant.feedbacks.length > 0) {
+				this.reviews = _.orderBy(applicant.feedbacks, ['publish_date'], ['desc']);
+	
+				_.each(this.reviews, (feedback) => {
+					feedback.publish_date = moment(feedback.publish_date,'YYYY-MM-DD HH:mm:ss').format('YYYY-MM-DD');
+					feedback.diff_months = moment().diff(feedback.publish_date, 'months');
+				})
 			}
-		];
+
+			console.log('reviews', this.reviews);
+		})
+		// this.reviews = [
+		// 	{
+		// 		name: '黃小明',
+		// 		publish_date: '2018-09-01',
+		// 		rating: 4,
+		// 		feedback: '員工做事盡責，態度友善有禮，會考慮再次僱用。'
+		// 	},
+		// 	{
+		// 		name: '黃小明',
+		// 		publish_date: '2018-08-23',
+		// 		rating: 3,
+		// 		feedback: '員工做事盡責，態度友善有禮，會考慮再次僱用。員工做事盡責，態度友善有禮，會考慮再次僱用。員工做事盡責，態度友善有禮，會考慮再次僱用。員工做事盡責，態度友善有禮，會考慮再次僱用。員工做事盡責，態度友善有禮，會考慮再次僱用。員工做事盡責，態度友善有禮，會考慮再次僱用。員工做事盡責，態度友善有禮，會考慮再次僱用。員工做事盡責，態度友善有禮，會考慮再次僱用。員工做事盡責，態度友善有禮，會考慮再次僱用。'
+		// 	},
+		// 	{
+		// 		name: '黃小明',
+		// 		publish_date: '2018-07-01',
+		// 		rating: 4,
+		// 		feedback: '員工做事盡責，態度友善有禮，會考慮再次僱用。'
+		// 	}
+		// ];
 
 		this.review_count = this.reviews.length;
 	}
